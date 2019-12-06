5db926531961b1d55902c63dcdb1a06683c319fc
DerivadaDX <derivadadx@gmail.com>
2019-12-05 23:12:05 -0300

    Se creó clase Cube y se movieron métodos propios

--- sketch.js
+++ sketch.js
@@ -7,18 +7,19 @@ let y = quarterSide, x = quarterSide;
 let rotate = false;
 let angle = 0;
 
+let cube;
 function setup() {
-	container = select('.container');
-	faces = selectAll('.face');
+	noCanvas();
 
-	prepareFixedSquares();
-	prepareContainer();
-	prepareFaces();
+	cube = new Cube();
+
+	cube.setParent(select('.base'));
+	// cube.show();
 }
 
 function draw() {
 	if (rotate) {
-		container.style('transform', rotateString(['x', 'y'], radians(++angle)));
+		cube._container.style('transform', rotateString(['x', 'y'], radians(++angle)));
 	}
 }
 
@@ -47,28 +48,39 @@ function rotateString(arr, rads) {
 	return str;
 }
 
-function prepareContainer() {
-	container.size(side, side);
-	container.center();
-}
+class Cube {
+	constructor(side) {
+		this._side = side || 400;
+		this._halfSide = this._side / 2;
+		this._quarterSide = this._side / 4;
 
-function prepareFaces() {
-	faces.forEach(face => {
-		face.size(halfSide, halfSide);
-		face.center();
-	});
+		this._container = createDiv().size(this._side, this._side).addClass('container');
 
-	select('.top').style('transform', `rotateX(-90deg) translateZ(-${quarterSide}px)`);
-	select('.bottom').style('transform', `rotateX(90deg) translateZ(-${quarterSide}px)`);
+		this._faces = [
+			'top', 'bottom',
+			'front', 'back',
+			'left', 'right'
+		].map(faceType => createDiv().size(this._halfSide, this._halfSide).addClass(`face ${faceType} border`).parent(this._container));
 
-	select('.back').style('transform', `translateZ(-${quarterSide}px)`);
-	select('.front').style('transform', `rotateX(180deg) translateZ(-${quarterSide}px)`);
+		
 
-	select('.left').style('transform', `rotateY(90deg) translateZ(-${quarterSide}px)`);
-	select('.right').style('transform', `rotateY(-90deg) translateZ(-${quarterSide}px)`);
-}
+	select('.top').style('transform', `rotateX(-90deg) translateZ(-${this._quarterSide}px)`);
+	select('.bottom').style('transform', `rotateX(90deg) translateZ(-${this._quarterSide}px)`);
 
-function prepareFixedSquares() {
-	selectAll('.fixed-square').forEach((fs) => fs.size(side, side));
-	select('#fs_2').style('transform', 'rotateY(90deg)');
+	select('.back').style('transform', `translateZ(-${this._quarterSide}px)`);
+	select('.front').style('transform', `rotateX(180deg) translateZ(-${this._quarterSide}px)`);
+
+	select('.left').style('transform', `rotateY(90deg) translateZ(-${this._quarterSide}px)`);
+	select('.right').style('transform', `rotateY(-90deg) translateZ(-${this._quarterSide}px)`);
+	}
+
+	setParent(el) {
+		this._container.parent(el).center();
+		this._faces.forEach(f => f.center());
+		return this;
+	}
+
+	show() {
+		this._container.show();
+	}
 }
